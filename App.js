8b1d326041ffb09894b6dd96867c6633b13fb1f7
Leandro Perez <Leandro_perez03@outlook.com>
2024-01-31 15:35:08 -0400

    Feature: Added clerk auth

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './App/Screens/LoginScreen/Login';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import Login from "./App/Screens/LoginScreen/Login";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Login />
-      <StatusBar style="auto" />
-    </View>
+    // add tokenCache later
+    <ClerkProvider publishableKey="pk_test_aW5ub2NlbnQtc3dpbmUtMzMuY2xlcmsuYWNjb3VudHMuZGV2JA">
+      <SafeAreaView style={styles.container}>
+        <SignedIn>
+          <Text>hola</Text>
+        </SignedIn>
+        <SignedOut>
+          <Login />
+        </SignedOut>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </ClerkProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingTop: 20,
   },
 });
--- App/Screens/LoginScreen/Login.jsx
+++ App/Screens/LoginScreen/Login.jsx
@@ -1,64 +1,99 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React from 'react'
-import Colors from '../../Utils/Colors'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import Colors from "../../Utils/Colors";
+import { maybeCompleteAuthSession } from "expo-web-browser";
+import { useWarmUpBrowser } from "../../hooks/warmUpBrowser";
+import { useOAuth } from "@clerk/clerk-expo";
+
+maybeCompleteAuthSession();
 
 export default function Login() {
+  useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+ 
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
   return (
-    <View style={{alignItems: 'center'}}>
-        <Image source={require('../../../assets/Images/login.png')} style={styles.loginImage}/>
-        <View style={styles.subContainer}>
-            <Text style={styles.mainText}>
-                Let's Find
-                <Text style={{fontWeight: 'bold'}}> Professional Cleaning and Repair </Text>
-                Service 
-            </Text>
-            <Text style={styles.secondaryText}>Best App to find services near you wich deliver you a professional service</Text>
-            <View style={styles.button}>
-                <Text style={{fontSize: 17, color: Colors.PRIMARY}}>Get Started</Text>
-            </View>
-        </View>
+    <View style={{ alignItems: "center" }}>
+      <Image
+        source={require("../../../assets/Images/login.png")}
+        style={styles.loginImage}
+      />
+      <View style={styles.subContainer}>
+        <Text style={styles.mainText}>
+          Let's Find
+          <Text style={{ fontWeight: "bold" }}>
+            {" "}
+            Professional Cleaning and Repair{" "}
+          </Text>
+          Service
+        </Text>
+        <Text style={styles.secondaryText}>
+          Best App to find services near you wich deliver you a professional
+          service
+        </Text>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <Text style={{ fontSize: 17, color: Colors.PRIMARY }}>
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    loginImage: {
-        width: 230,
-        height: 450,
-        marginTop: 70,
-        borderWidth: 4,
-        borderColor: Colors.BLACK,
-        borderRadius: 15
-    },
+  loginImage: {
+    width: 230,
+    height: 450,
+    marginTop: 70,
+    borderWidth: 4,
+    borderColor: Colors.BLACK,
+    borderRadius: 15,
+  },
 
-    subContainer: {
-        width: '100%',
-        backgroundColor: Colors.PRIMARY,
-        height: '70%',
-        marginTop: -20,
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        padding: 20,
-    },
+  subContainer: {
+    width: "100%",
+    backgroundColor: Colors.PRIMARY,
+    height: "70%",
+    marginTop: -20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    padding: 20,
+  },
 
-    mainText: {
-        textAlign: 'center',
-        color: Colors.WHITE,
-        fontSize: 27
-    },
+  mainText: {
+    textAlign: "center",
+    color: Colors.WHITE,
+    fontSize: 27,
+  },
 
-    secondaryText: {
-        fontSize: 17,
-        color: Colors.WHITE,
-        textAlign: 'center',
-        marginTop: 20
-    },
+  secondaryText: {
+    fontSize: 17,
+    color: Colors.WHITE,
+    textAlign: "center",
+    marginTop: 20,
+  },
 
-    button: {
-        backgroundColor: Colors.WHITE,
-        padding: 15,
-        borderRadius: 99,
-        alignItems: 'center',
-        marginTop: 40
-    }
-})
+  button: {
+    backgroundColor: Colors.WHITE,
+    padding: 15,
+    borderRadius: 99,
+    alignItems: "center",
+    marginTop: 40,
+  },
+});
